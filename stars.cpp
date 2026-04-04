2da5fce3c021aef79667f03bd1fb32013fc0b57b
Sanskriti Gupta <146509881+sanskriti5002@users.noreply.github.com>
2026-04-04 15:46:09 +0530

    Create stars.cpp

---
+++ stars.cpp
@@ -0,0 +1,15 @@
+#include<simplecpp>
+main_program(){
+    int N;
+    cin >> N;
+    repeat(N){
+        repeat(5){
+            cout << "*";
+
+        }
+        cout <<endl;
+
+    }
+    return 0;
+    
+}
