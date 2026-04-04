1fedf5e7e5d01d7bbcd81b794ff646999db6041e
Sanskriti Gupta <146509881+sanskriti5002@users.noreply.github.com>
2026-04-04 15:45:39 +0530

    Create learning.cpp

---
+++ learning.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    for(int i =0;i<N;i++){
+        cout << " I am learning C++" << endl;
+
+    }
+    return 0;
+}
